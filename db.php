40efe1ffc2647c6efdb3e2fa94b55bd9f7bc5953
Bethel <Beamen480@yahoo.com>
2022-09-05 18:44:38 +0100

    changed db name

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","bethel_database");
+$con = mysqli_connect("localhost","root","","agritech_database");
     if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	die();
