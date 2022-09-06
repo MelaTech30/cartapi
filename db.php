aae5217650d1416474a231a73beb2a41f001d517
Bethel <Beamen480@yahoo.com>
2022-09-06 17:43:06 +0100

    username

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("	sql213.epizy.com","root","QmGORJtpAL5","epiz_32537422_agritech_database");
+$con = mysqli_connect("	sql213.epizy.com","	epiz_32537422","QmGORJtpAL5","epiz_32537422_agritech_database");
 if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	die();
